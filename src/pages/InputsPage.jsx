716f38056e1375d7520fda8cf55a5baad8f32624
Saachi Bhayani <saachi.bhayani@gmail.com>
2024-05-21 14:25:18 -0700

    added header with dropdown menu

--- src/pages/InputsPage.jsx
+++ src/pages/InputsPage.jsx
@@ -4,15 +4,22 @@ export default function InputsPage() {
   return (
     <div>
     <div className="ps-1rem">
-    
-        <pre className="leading-[2rem]">
-          By helping Bruins help Bruins navigate on-campus dining.{"\n"}
-          We offer a digital space for students to review UCLA’s food {"\n"}
-          trucks and benefit current and future students alike.
-        </pre>
-        <div className="btn bg-transparent border-none">
-          hi
-        </div>
+    <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>Saved Trips</a></li>
+        <li><a>Account</a></li>
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost text-xl">Hermes</a>
+  </div>
+</div>
         
       </div>
       
